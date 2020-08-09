625978f07d801e2511dff832cfac63af85b5e469
Zachary Zhu <zach.z3394@gmail.com>
2020-08-08 21:55:38 -0400

    Add New Cards

--- backend/src/types/Game.ts
+++ backend/src/types/Game.ts
@@ -1,4 +1,4 @@
-import { GameCard, makeDeck } from './Cards';
+import { GameCard, makeDeck, shuffleDeck } from './Cards';
 import { GameEvent } from './Events';
 
 export enum Role {
@@ -6,48 +6,42 @@ export enum Role {
   VICE = 'VICE',
   RENEGADE = 'RENEGADE',
   OUTLAW = 'OUTLAW',
-  NONE = 'NONE'
 }
 
 export class Player {
-
   id: String;
   name: String;
-  role: Role;
+  role?: Role;
+  character?: Character;
   maxHealth: Number;
   currentHealth: Number;
+  alive: Boolean;
   cards: {
     hand: GameCard[],
     board: GameCard[],
-  }
+  };
 
-  alive: Boolean;
-  // character: Character;
-
-  constructor (id: String, name: String) {
+  constructor(id: String, name: String) {
     this.id = id;
     this.name = name;
-    this.role = Role.NONE; 
     this.maxHealth = -1;
     this.currentHealth = -1;
-    this.cards = {hand: [], board: []}
+    this.cards = { hand: [], board: [] };
     this.alive = true;
-    // this.character = ??
   }
 }
 
 export interface CharacterEffect {
-  event: GameEvent,
-  func: (data: any) => Event[]
+  event: GameEvent;
+  func: (data: any) => Event[];
 }
 
 export class Character {
-
   name: String;
   text: String;
-  effect: CharacterEffect[]
+  effect: CharacterEffect[];
 
-  constructor (name: String, text: String, effect: CharacterEffect[]) {
+  constructor(name: String, text: String, effect: CharacterEffect[]) {
     this.name = name;
     this.text = text;
     this.effect = effect;
@@ -57,23 +51,21 @@ export class Character {
 export enum GameState {
   WAITING = 'WAITING',
   PLAYING = 'PLAYING',
-  COMPLETED = 'COMPLETED'
+  COMPLETED = 'COMPLETED',
 }
 
 export class Game {
-
   id: String;
   deck: GameCard[];
   discard: GameCard[];
   turn: String;
   state: GameState;
 
-  constructor (id: String) {
+  constructor(id: String) {
     this.id = id;
-    this.deck = makeDeck();
+    this.deck = shuffleDeck(makeDeck());
     this.discard = [];
-    this.turn = '',
-    this.state = GameState.WAITING
+    this.turn = '';
+    this.state = GameState.WAITING;
   }
-  
 }
