ea3b2a9b008c1d05bde3153bb36ed45de5be9f31
David Shen <me@pantherman594.com>
2020-08-09 20:11:44 -0400

    Move types to a "common" package

---
+++ common/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'airbnb-typescript/base'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    project: './tsconfig.json',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'max-classes-per-file': 'off',
+    '@typescript-eslint/lines-between-class-members': [ 'warn', 'always', { exceptAfterSingleLine: true } ],
+    '@typescript-eslint/no-unused-vars': [ 'error', { 'argsIgnorePattern': '^_' } ],
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
---
+++ common/src/__tests__/Player.test.ts
@@ -0,0 +1,31 @@
+import { Player } from '../Player';
+
+describe('Player', () => {
+  it('initializes', () => {
+    const id = 'abcd';
+    const name = 'jeff';
+
+    const player = new Player(id, name);
+
+    expect(player.id).toBe(id);
+    expect(player.name).toBe(name);
+    expect(player.maxHealth).toBe(-1);
+    expect(player.currentHealth).toBe(-1);
+    expect(player.cards.hand.length).toBe(0);
+    expect(player.cards.board.length).toBe(0);
+    expect(player.alive).toBeTruthy();
+    expect(player.range).toBe(1);
+    expect(player.rangeMod).toBe(0);
+    expect(player.distanceMod).toBe(0);
+    expect(player.hasBanged).toBeFalsy();
+    expect(player.barrel).toBeFalsy();
+    expect(player.mustang).toBeFalsy();
+    expect(player.scope).toBeFalsy();
+    expect(player.volcanic).toBeFalsy();
+    expect(player.jail).toBeFalsy();
+    expect(player.dynamite).toBeFalsy();
+
+    expect(player.role).toBeUndefined();
+    expect(player.character).toBeUndefined();
+  });
+});
