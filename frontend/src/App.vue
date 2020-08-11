a0eaa23cd8854f9788a3f87cbe1c3fd68430d78c
David Shen <me@pantherman594.com>
2020-08-11 00:05:54 -0400

    Add emotion in case we want to use it

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,23 @@
 <template>
-  <div id='app'>
+  <div id='app' :class='styles.app'>
     <router-view />
   </div>
 </template>
 
 <script lang='ts'>
 import { Vue } from 'vue-property-decorator';
+import { css } from 'emotion';
 
-export default class App extends Vue {}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+export default class App extends Vue {
+  styles = {
+    app: css({
+      fontFamily: 'Avenir, Helvetica, Arial, sans-serif',
+      webkitFontSmoothing: 'antialiased',
+      mozOsxFontSmoothing: 'grayscale',
+      textAlign: 'center',
+      color: '#2c3e50',
+      marginTop: 60,
+    }),
+  };
 }
-</style>
+</script>
