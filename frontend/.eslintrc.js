7e28cc1a3c7d8a1493be4cc98107e108ad23b95a
David Shen <me@pantherman594.com>
2020-08-12 01:41:07 -0400

    Don't build common

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -10,7 +10,6 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 2020,
-    project: './tsconfig.json',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
