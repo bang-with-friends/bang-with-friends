896b7afc7dbbc69b769271db29b50126ca42e651
David Shen <me@pantherman594.com>
2020-08-12 01:36:07 -0400

    sigh

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 2020,
+    project: './tsconfig.json',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
